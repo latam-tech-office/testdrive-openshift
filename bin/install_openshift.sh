33e60267d427d56467d4abda69a31b9f644a072e
Mauricio Leal <maltron@gmail.com>
2017-05-21 12:00:06 -0300

    Creating a new user into Dev environment

--- bin/install_openshift.sh
+++ bin/install_openshift.sh
@@ -1,1 +1,1 @@
-ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@35.185.116.240 "sudo ansible-playbook /usr/share/ansible/openshift-ansible/playbooks/byo/config.yml; ansible-playbook /root/post-install-openshift.yaml"
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@35.185.14.133 "sudo ansible-playbook /usr/share/ansible/openshift-ansible/playbooks/byo/config.yml; ansible-playbook /root/post-install-openshift.yaml"
