ddbc7a811252da2bfb69490bf83a9bd7513fad98
root <root@bastion.c.latam-development-testdriveocp.internal>
2018-04-09 16:54:28 -0400

    Changed to bluegreen-example.cloudapps.testdrive.com

--- labs/cd/release/bluegreen/pinging.sh
+++ labs/cd/release/bluegreen/pinging.sh
@@ -2,7 +2,7 @@
 
 while true 
 do
-curl --connect-timeout 1 -s http://application-ab.cloudapps.testdrive.com/ws/whoami/;
+curl --connect-timeout 1 -s http://bluegreen-example.cloudapps.testdrive.com/ws/whoami/;
 echo ;
 sleep 1;
 done
