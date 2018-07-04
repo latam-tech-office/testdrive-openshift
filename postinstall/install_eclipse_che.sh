85ecbb78970fbade952c0847c164c1471867f611
Mauricio "Maltron" Leal <maltron@gmail.com>
2018-07-04 14:43:42 -0300

    Removed quotes from display-name

--- postinstall/install_eclipse_che.sh
+++ postinstall/install_eclipse_che.sh
@@ -1,4 +1,4 @@
-oc adm new-project che --display-name=\"Eclipse Che\" --admin=demo
+oc adm new-project che --display-name="Eclipse Che" --admin=demo
 git clone https://github.com/latam-tech-office/openshift-utils
 pushd openshift-utils/eclipseche/
 ansible-playbook install-eclipse-che.yaml -e che_routing_suffix=cloudapps.testdrive.com
